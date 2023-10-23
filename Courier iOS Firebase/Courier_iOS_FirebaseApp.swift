803e7ab08dcf5fb91b91e6dc22c8b4a3e48cc373
Carter Rabasa <carter.rabasa@gmail.com>
2023-10-23 13:40:44 -0700

    Initial Commit

---
+++ Courier iOS Firebase/Courier_iOS_FirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Courier_iOS_FirebaseApp.swift
+//  Courier iOS Firebase
+//
+//  Created by Carter Rabasa on 10/23/23.
+//
+
+import SwiftUI
+
+@main
+struct Courier_iOS_FirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
